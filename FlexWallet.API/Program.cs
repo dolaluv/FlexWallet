28d4fe5daefbdee725050df3344a768ecba16401
dolapo <midtech4info@gmail.com>
2022-10-04 16:10:38 +0100

    service registration

--- FlexWallet.API/Program.cs
+++ FlexWallet.API/Program.cs
@@ -1,3 +1,9 @@
+using FlexWallet.Abstractions.Helper;
+using FlexWallet.Abstractions.Services.Business;
+using FlexWallet.Abstractions.Services.Data;
+using FlexWallet.Data.Mock;
+using FlexWallet.Data.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +14,36 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IWalletTransactionService, WalletTransactionService>();
+
+var getSection = builder.Configuration;
+var appSettingsSection = getSection.GetSection("AppSettings");
+var appSettings = appSettingsSection.Get<AppSettings>();
+
+
+//IMapper mapper = MapConfig.RegristerMapper().CreateMapper();
+//builder.Services.AddSingleton(mapper);
+
+if (appSettings.UseMock)
+{
+
+    builder.Services.AddScoped<IAccountDataService, MockAccountDataService>();
+    builder.Services.AddScoped<IWalletTransactionDataService, MockWalletTransactionDataService>();
+
+
+}
+else
+{
+
+    builder.Services.AddScoped<IAccountDataService, AccountDataService>();
+    builder.Services.AddScoped<IWalletTransactionDataService, WalletTransactionDataService>();
+
+}
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
